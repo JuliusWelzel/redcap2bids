1e944b71a5c2a7eb097444385d16305b88edd716
jwelzel <52565341+JuliusWelzel@users.noreply.github.com>
2025-08-25 09:36:11 +0200

    Remove unused dotenv import from instrument2redcap.py and dependencies in pyproject.toml

--- scripts/instrument2redcap.py
+++ scripts/instrument2redcap.py
@@ -1,4 +1,3 @@
-from dotenv import load_dotenv
 import pandas as pd
 from redcap import Project
 import os
@@ -9,7 +8,6 @@ from rcol.instruments import fal, ehi
 ####
 # Update RedCap project structure
 ####
-load_dotenv()
 RC_API_KEY = os.getenv("RC_API_KEY")
 api_url = 'https://redcapdev.uol.de/api/'
 rc_project = Project(api_url, RC_API_KEY)
